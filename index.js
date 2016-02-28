99bd97e3aea4ac6159bc2d07c634bd2a1bd31fca
Jose Manuel Moreno Triguero <jose.manuel.moreno.triguero@gmail.com>
2016-02-28 19:23:04 +0100

    Descripcion

--- index.js
+++ index.js
@@ -3,13 +3,44 @@ var express = require("express");
 var app = express();
 
 
-app.get("/",(req,res) => {
+app.get("/about",(req,res) => {
 
 
- res.write("Hola mundo");
+ res.write("Grupo 4");
 
 res.end();
 
 });
+
+
+app.get("/about/population-percentage-by-age",(req,res) => {
+
+
+ res.write("Aqui iria la fuente de datos de Moreno");
+
+res.end();
+
+});
+
+
+app.get("/about/fuente-de-datos-de-alex",(req,res) => {
+
+
+ res.write("Aqui iria la fuente de datos de Alex");
+
+res.end();
+
+});
+
+
+app.get("/about/fuente-de-datos-de-jesus",(req,res) => {
+
+
+ res.write("Aqui iria la fuente de datos de Jesus");
+
+res.end();
+
+});
+
 
 app.listen(process.env.PORT);
