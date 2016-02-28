e6352654a0de82a82274cd4f9e391d32c131ebf8
chusgarcia <jesusgarciasanchez92@hotmail.com>
2016-02-28 15:00:10 +0100

    final. Closes #1

--- pr.js
+++ pr.js
@@ -4,7 +4,7 @@ var app=express();
 app.get("/about/Porcentaje_desempleo_por_sexo", (req,res) =>{
 res.write("<html><body>____ Porcentaje de desempleo ____<ul>");
 
-res.write("</li>Se tendra en cuenta el sexo para comparar los datos de las siguientes regiones:spain,argentina,canada,france,germany,sweden</li>");
+res.write("</li>Se tendra en cuenta el  sexo para comparar los datos de las siguientes regiones:spain,argentina,canada,france,germany,sweden</li>");
 
 
 
