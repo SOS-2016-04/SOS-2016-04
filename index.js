6057fd12b5654bcf6b0794e51303619d603b74ed
Jose Manuel Moreno Triguero <jose.manuel.moreno.triguero@gmail.com>
2016-02-28 19:56:03 +0100

    Descripcion

--- index.js
+++ index.js
@@ -11,11 +11,11 @@ app.get("/about",(req,res) => {
 
  res.write("GROUP 04 ---- MEMBERS AND DATA SOURCES\n\n");
 
- res.write("Jose Manuel Moreno Triguero -> <a>enla</a>\n");
+ res.write("Jose Manuel Moreno Triguero -> population-percentage-by-age\n");
 
- res.write(" -> \n");
+ res.write("Alejandro Rodriguez Caro -> \n");
 
- res.write(" -> \n");
+ res.write("Jesus Garcia Sanchez -> \n");
 
 res.end();
 
@@ -25,7 +25,10 @@ res.end();
 app.get("/about/population-percentage-by-age",(req,res) => {
 
 
- res.write("Aqui iria la fuente de datos de Moreno");
+ res.write("This data source analyze population percentage between 0 and 14 years ");
+ res.write("and between 15 and 64 years filtered by country and year. Examples: \n\n");
+ res.write("country: spain --- year: 2014 --- population-0-14: 15% ---population-15-64: 67%");
+ 
 
 res.end();
 
