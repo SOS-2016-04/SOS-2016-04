c1f4b643dd6e548815d662d7fc6ae375756cbc71
alerc91 <alejandro.rc.1@gmail.com>
2016-02-29 18:44:46 +0100

    application deployment closed #5

--- index.js
+++ index.js
@@ -7,13 +7,13 @@ app.get("/about",(req,res) => {
 
  res.write("GROUP 04 ---- THEMATIC\n\n");
 
- res.write("Our data sources are aimed to analyze relationship between population age, population activity and unemployment\n\n\n\n");
+ res.write("Our data sources are aimed to analyze relationship between population age, population workforce and unemployment\n\n\n\n");
 
  res.write("GROUP 04 ---- MEMBERS AND DATA SOURCES\n\n");
 
  res.write("Jose Manuel Moreno Triguero -> population-percentage-by-age\n");
 
- res.write("Alejandro Rodriguez Caro -> \n");
+ res.write("Alejandro Rodriguez Caro -> population-labor-force-percentage-by-education\n");
 
  res.write("Jesus Garcia Sanchez -> population-unemployed-percentage-by-gender\n");
 
@@ -35,10 +35,13 @@ res.end();
 });
 
 
-app.get("/about/fuente-de-datos-de-alex",(req,res) => {
+app.get("/about/population-labor-force-percentage-by-education",(req,res) => {
 
 
- res.write("Aqui iria la fuente de datos de Alex");
+  res.write("This data source analyze population percentage labor force by education (Primary, secondary and tertiary). \n\n ");
+  res.write("Examples: \n\n");
+  res.write("country: spain --- year: 2014 --- primary-education: 39% --- secondary-education: 23% --- tertiary-education: 37%");
+
 
 res.end();
 
