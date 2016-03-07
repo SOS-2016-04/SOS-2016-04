83a0d68c9a84e0f14076bf06322bc5e426f55998
Jose Manuel Moreno Triguero <jose.manuel.moreno.triguero@gmail.com>
2016-03-07 14:35:38 +0100

    asdf

--- index.js
+++ index.js
@@ -2,63 +2,18 @@ var express = require("express");
 
 var app = express();
 
+var port = (process.env.PORT || 10000);
 
-app.get("/about",(req,res) => {
+app.use("/",express.static(__dirname+"/static"));
 
- res.write("GROUP 04 ---- THEMATIC\n\n");
 
- res.write("Our data sources are aimed to analyze relationship between population age, population workforce and unemployment\n\n\n\n");
+app.get("/time", (req,res)=>{
 
- res.write("GROUP 04 ---- MEMBERS AND DATA SOURCES\n\n");
-
- res.write("Jose Manuel Moreno Triguero -> population-percentage-by-age\n");
-
- res.write("Alejandro Rodriguez Caro -> population-labor-force-percentage-by-education\n");
-
- res.write("Jesus Garcia Sanchez -> population-unemployed-percentage-by-gender\n");
-
-res.end();
+var now = new Date();
+res.send("It is "+now);
 
 });
 
 
-app.get("/about/population-percentage-by-age",(req,res) => {
 
-
- res.write("This data source analyze population percentage between 0 and 14 years ");
- res.write("and between 15 and 64 years filtered by country and year. Examples: \n\n");
- res.write("country: spain --- year: 2014 --- population-0-14: 15% ---population-15-64: 67%");
-
-
-res.end();
-
-});
-
-
-app.get("/about/population-labor-force-percentage-by-education",(req,res) => {
-
-
-  res.write("This data source analyze population percentage labor force by education (Primary, secondary and tertiary). \n\n ");
-  res.write("Examples: \n\n");
-  res.write("country: spain --- year: 2014 --- primary-education: 39% --- secondary-education: 23% --- tertiary-education: 37%");
-
-
-res.end();
-
-});
-
-
-app.get("/about/population-unemployed-percentage-by-gender", (req,res) =>{
-
-
-  res.write("This data source analyze population percentage unemployed by gender. \n\n");
-  res.write("Examples: \n\n");
-  res.write("country: spain --- year: 2014 --- female unemployed:26 --- male unemployed:23.7");
-
-
-
-res.end();
-});
-
-
-app.listen(process.env.PORT);
+app.listen(port);
