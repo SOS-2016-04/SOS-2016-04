86874408c7e236b0fa030773a6207e971b6fb936
Jose Manuel Moreno Triguero <jose.manuel.moreno.triguero@gmail.com>
2016-03-11 19:03:55 +0100

    dsffds

--- index.js
+++ index.js
@@ -10,11 +10,7 @@ app.use("/",express.static(__dirname+"/static"));
 app.get("/time", (req,res)=>{
 
 var now = new Date();
-<<<<<<< HEAD
-res.send("It is  "+now);
-=======
 res.send("It is "+now);
->>>>>>> 5d2b2cc741e9c738289ada3672524d3318a46d2d
 
 });
 
