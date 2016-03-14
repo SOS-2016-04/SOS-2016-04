4a2dcf0097b8619aef6172d201eebe705722cf21
chusgarcia <jesusgarciasanchez92@hotmail.com>
2016-03-14 15:21:24 +0100

    asg

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ var port = (process.env.PORT || 10000);
 app.use(bodyParser.json());
 
 var football_teams = [];
+var movies = [];
+
+
 
 app.use("/",express.static(__dirname+"/static"));
 
@@ -109,6 +112,95 @@ app.put("/api/sandbox/football-teams/:name", (req,res)=> {
     }
 });
 
+
+app.get("/api-test/movies/loadInitialData",(req,res) => {
+	movies=[{name:"ironman", year:"2008" , id:247},
+	                {name:"gladiator", year:"2000", id:275}];
+   res.send("Datos inicializados.");
+});
+
+
+app.get("/api/sandbox/movies",(req,res) => {
+	res.send(movies);
+});
+
+app.get("/api/sandbox/movies/:name",(req,res) => {
+	var title = req.params.name;
+	var found = false;
+	for(var i=0;i<movies.length;i++)
+	{
+		if(movies[i].name == title)
+		{
+			res.send(movies[i]);
+			found = true;
+		}
+    }
+    if(found == false)
+    {
+	res.sendStatus(404);
+    }
+});
+
+
+app.post("/api/sandbox/movies", (req,res)=> {
+	var movie = req.body;
+	movies.push(movie);
+	res.send("Datos introducidos correctamente.");
+});
+
+
+app.post("/api/sandbox/movies/:equipo", (req,res)=> {
+	res.send("Error. Metodo no permitido.");
+});
+
+
+app.delete("/api/sandbox/movies", (req,res)=> {
+	movies = [];
+	res.send("Datos borrados correctamente.");
+});
+
+
+app.delete("/api/sandbox/football-teams/:name", (req,res)=> {
+	var title = req.params.name;
+	var found = false;
+	for(var i=0;i<movies.length;i++)
+	{
+		if(movies[i].name == title)
+		{
+			movies.splice(i,1);
+			found = true;
+			res.send("Datos borrados correctamente.")
+		}
+    }
+    if(found == false)
+    {
+	res.sendStatus(404);
+    }
+});
+
+
+app.put("/api/sandbox/movies", (req,res)=> {
+	res.send("Error. Metodo no permitido");
+});
+
+
+app.put("/api/sandbox/movies/:name", (req,res)=> {
+	var title = req.params.name;
+	var found = false;
+	for(var i=0;i<movies.length;i++)
+	{
+		if(movies[i].name == title)
+		{
+			movies[i] = req.body;
+			found = true;
+			res.send("Datos introducidos correctamente.");
+		}
+    }
+    if(found == false)
+    {
+	res.sendStatus(404);
+    }
+});
 
 
 app.listen(port);
