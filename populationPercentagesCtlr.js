0e09649d9b35fd168b2d5c4869d64867b9246d75
Jose Manuel Moreno Triguero <jose.manuel.moreno.triguero@gmail.com>
2016-03-22 22:02:16 +0100

    sdfs

---
+++ populationPercentagesCtlr.js
@@ -0,0 +1,100 @@
+
+
+var population_percentages = [];
+
+module.exports.loadInitialData = function(req,res){
+football_teams=[{country: "spain", year: 2014, population-0-14: 15, population-15-64: 67},
+{country: "spain", year: 2013, population_0_14: 14, population_15_64: 68},
+{country: "canada", year: 2014, population_0_14: 16, population_15_64: 68},
+{country: "france", year: 2012, population_0_14: 19, population_15_64: 63},
+{country: "france", year: 2013, population_0_14: 15, population_15_64: 64},
+{country: "sweden", year: 2014, population_0_14: 17, population_15_64: 63}];
+}
+
+
+module.exports.getPopulationPercentages = function(req,res){
+res.send(population_percentages);
+};
+
+
+/*
+module.exports.getPopulationPercentage = function(req,res){
+var country = req.params.country;
+	var encontrado = false;
+	for(var i=0;i<football_teams.length;i++)
+	{
+		if(football_teams[i].name == team_name)
+		{
+			res.send(football_teams[i]);
+			encontrado = true;
+		}
+    }
+    if(encontrado == false)
+    {
+	res.sendStatus(404);
+    }
+}
+
+
+module.exports.postFootballTeam = function(req,res){
+res.send("Error. Metodo no permitido.");
+}
+
+
+module.exports.postFootballTeams = function(req,res){
+var team = req.body;
+	football_teams.push(team);
+	res.send("Datos introducidos correctamente.");
+}
+
+
+module.exports.deleteFootballTeam = function(req,res){
+var team_name = req.params.name;
+	var encontrado = false;
+	for(var i=0;i<football_teams.length;i++)
+	{
+		if(football_teams[i].name == team_name)
+		{
+			football_teams.splice(i,1);
+			encontrado = true;
+			res.send("Datos borrados correctamente.")
+		}
+    }
+    if(encontrado == false)
+    {
+	res.sendStatus(404);
+    }
+}
+
+
+module.exports.deleteFootballTeams = function(req,res){
+ football_teams = [];
+ res.send("Datos borrados correctamente.");
+}
+
+
+module.exports.putFootballTeam = function(req,res){
+var team_name = req.params.name;
+	var encontrado = false;
+	for(var i=0;i<football_teams.length;i++)
+	{
+		if(football_teams[i].name == team_name)
+		{
+			football_teams[i] = req.body;
+			encontrado = true;
+			res.send("Datos introducidos correctamente.");
+		}
+    }
+    if(encontrado == false)
+    {
+	res.sendStatus(404);
+    }
+}
+
+
+module.exports.putFootballTeams = function(req,res){
+ res.send("Error. Metodo no permitido");
+}
+
+
+*/
