2d6b5448fb074cfe310cfcfc741c43d8a1b44b86
chusgarcia <jesusgarciasanchez92@hotmail.com>
2016-03-25 14:28:03 +0100

    dsa

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var bodyParser = require("body-parser");
 
 var populationPercentagesCtlr = require('./populationPercentagesCtlr');
 
-//var unemployedCtlr = require('./unemployedCtlr');
+var unemployedCtlr = require('./unemployedCtlr');
 
 var app = express();
 
@@ -52,7 +52,7 @@ app.put("/api/v1/population-percentage-by-age/:country/:year",populationPercenta
 
 //////////////      Unemployed       /////////////
 
-app.get("/api/v1/population-unemployed-percentage-by-gender/loadInitialData",populationPercentagesCtlr.loadInitialData2);
+app.get("/api/v1/population-unemployed-percentage-by-gender/loadInitialData",unemployedCtlr.loadInitialData2);
 
 /*app.get("/api/v1/population-unemployed-percentage-by-gender",unemployedCtlr.getUnemployes);
 
