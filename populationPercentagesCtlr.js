111e42040245b17de2a4ff56cb80338097e2a5d6
Jose Manuel Moreno Triguero <jose.manuel.moreno.triguero@gmail.com>
2016-04-01 00:41:58 +0200

    sdf

--- populationPercentagesCtlr.js
+++ populationPercentagesCtlr.js
@@ -536,7 +536,8 @@ var peticion_valida = true;
 if (apikey && apikey=="secret")
 {
 
-if(population_percentage.country === undefined || population_percentage.year === undefined || population_percentage.population_0_14 === undefined || population_percentage.population_15_64 === undefined || isNaN(population_percentage.year) || isNaN(population_percentage.population_0_14)  || isNaN(population_percentage.population_15_64))
+
+if(population_percentage.country === undefined || population_percentage.year === undefined || population_percentage.population_0_14 === undefined || population_percentage.population_15_64 === undefined || isNaN(population_percentage.year) || isNaN(population_percentage.population_0_14)  || isNaN(population_percentage.population_15_64) || isNaN(population_percentage.country) == false)
 {
 	peticion_valida = false;
 }
