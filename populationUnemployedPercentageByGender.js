ea8b3b763b6e11ecf06d8fc07db181a5f2e8958f
chusgarcia <jesusgarciasanchez92@hotmail.com>
2016-05-09 16:00:39 +0200

    asd

--- populationUnemployedPercentageByGender.js
+++ populationUnemployedPercentageByGender.js
@@ -71,6 +71,10 @@ else
 
 
 module.exports.getUnemploye = function(req,res){
+	from = req.query.from;
+	to = req.query.to;
+	limit = req.query.limit;
+	offset = req.query.offset;
 	apikey = req.query.apikey;
 var data = req.params.data;
 var resultado = [];
@@ -93,6 +97,16 @@ var resultado = [];
 
     }
 
+		if (from && to)
+		{
+		for(var i=0;i<resultado.length;i++)
+		{
+		if(resultado[i].year < from  ||  resultado[i].year > to)
+		{
+			resultado.splice(i,1);
+			i = i - 1;
+		}}}
+
     if(encontrado == true)
     {
     	res.send(resultado);
@@ -109,6 +123,10 @@ var resultado = [];
 
 
 module.exports.getUnemployeCountryYear = function(req,res){
+	from = req.query.from;
+	to = req.query.to;
+	limit = req.query.limit;
+	offset = req.query.offset;
 	apikey = req.query.apikey;
 var country = req.params.country;
 var year = req.params.year;
@@ -125,6 +143,17 @@ var resultado = [];
 			encontrado = true;
 		}
     }
+
+		if (from && to)
+		{
+		for(var i=0;i<resultado.length;i++)
+		{
+		if(resultado[i].year < from  ||  resultado[i].year > to)
+		{
+			resultado.splice(i,1);
+			i = i - 1;
+		}}}
+
 
     if(encontrado == true)
     {
