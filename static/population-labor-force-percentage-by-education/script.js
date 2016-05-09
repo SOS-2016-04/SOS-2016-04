ca0fcc3398f0dccb758542ab5444e3b5028558c2
alerc91 <alejandro.rc.1@gmail.com>
2016-05-09 14:14:43 +0200

    Ale

--- static/population-labor-force-percentage-by-education/script.js
+++ static/population-labor-force-percentage-by-education/script.js
@@ -667,7 +667,7 @@ function busqueda(){
                     urlBusqueda='http://sos-2016-04.herokuapp.com/api/v1/population-labor-force-percentage-by-education/'+busqueda+'/'+busqueda2+'?apikey=' + $("#apikey").val()+'&to='+busqueda3;
             }else if(busqueda2!='' && busqueda3!='' && busqueda!=''){
                     urlBusqueda='http://sos-2016-04.herokuapp.com/api/v1/population-labor-force-percentage-by-education/'+busqueda+'?apikey=' + $("#apikey").val() + '&from=' + busqueda2 + '&to=' + busqueda3;
-            }else if(busqueda2!='' && busqueda3!='' && busqueda''){
+            }else if(busqueda2!='' && busqueda3!='' && busqueda=''){
                     urlBusqueda='http://sos-2016-04.herokuapp.com/api/v1/population-labor-force-percentage-by-education/?apikey=' + $("#apikey").val() + '&from=' + busqueda2 + '&to=' + busqueda3;
             }else if(busqueda2=='' && busqueda3!='' && busqueda==''){
                     urlBusqueda='http://sos-2016-04.herokuapp.com/api/v1/population-labor-force-percentage-by-education/?apikey=' + $("#apikey").val() +  '&to=' + busqueda3;
