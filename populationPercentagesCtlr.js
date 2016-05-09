da2260173a9ca326dbcec425712962d235af1a68
Jose Manuel Moreno Triguero <jose.manuel.moreno.triguero@gmail.com>
2016-05-09 23:25:32 +0200

    sdf

--- populationPercentagesCtlr.js
+++ populationPercentagesCtlr.js
@@ -122,8 +122,8 @@ if (apikey && apikey=="secret")
 
     	for(var i3=0;i3<resultado.length;i3++)
     	{
-    		//console.log(from === undefined + isNaN(from) + from <= resultado[i3] + to === undefined + isNaN(to) + to >= resultado[i3]);
-    		res.send(from === undefined);
+    		//console.log( isNaN(from) + from <= resultado[i3] + isNaN(to) + to >= resultado[i3]);
+    		res.send(from <= resultado[i3]);
 
 
     		if (from === undefined || isNaN(from) || from <= resultado[i3] || to === undefined || isNaN(to) || to >= resultado[i3])
