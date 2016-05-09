a56e51129f360f43a36138f4749336eb21812896
alerc91 <alejandro.rc.1@gmail.com>
2016-05-09 04:04:31 +0200

    Ale

--- static/population-labor-force-percentage-by-education/script.js
+++ static/population-labor-force-percentage-by-education/script.js
@@ -536,9 +536,9 @@ function editardato(){
   var a= $("#primaryEducation").val()
   var m= $("#secondaryEducation").val()
   var w= $("#tertiaryEducation").val()
-  var datos='{"'"country"'":"'+r+'","'"year"'":"'+y+'","'"primaryEducation"''":"'+a+'","'"secondaryEducation"''":"'+m+'","'"tertiaryEducation"'":"'+w+'"}';
+  var datos='{"country":"'+r+'","year":"'+y+'","primaryEducation":"'+a+'","secondaryEducation":"'+m+'","tertiaryEducation":"'+w+'"}';
   var metodo = "PUT";
-  var url = 'http://sos-2016-04.herokuapp.com//api/v1/population-labor-force-percentage-by-education/'+country+'/'+year+'?apikey='+$("#apikey").val();
+  var url = 'http://sos-2016-04.herokuapp.com/api/v1/population-labor-force-percentage-by-education/'+country+'/'+year+'?apikey='+$("#apikey").val();
     var request2 = $.ajax({
     url: url,
     type: metodo,
