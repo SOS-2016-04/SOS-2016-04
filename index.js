7dae3b881fa97521a0eac1b10d9da39bbaa34057
chusgarcia <jesusgarciasanchez92@hotmail.com>
2016-05-16 15:38:56 +0200

    asd

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var request = require("request");
-var cors=require('cors');
+//var cors=require('cors');
 var populationPercentagesCtlr = require('./populationPercentagesCtlr');
 var populationLaborForcePercentage = require('./populationLaborForcePercentage');
 var populationUnemployedPercentageByGender = require('./populationUnemployedPercentageByGender');
